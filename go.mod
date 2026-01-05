48748ae164b930cf6baf4abb99042e96f974a1e3
SerhiiKhyzhko <63621545+SerhiiKhyzhko@users.noreply.github.com>
2026-01-05 13:32:24 +0200

    Update and improve rest error lib

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/SerhiiKhyzhko/bookstore_utils-go
 
 go 1.23.6
 
-require github.com/go-playground/assert/v2 v2.2.0
+require (
+	github.com/go-playground/assert/v2 v2.2.0
+	go.uber.org/zap v1.27.1
+)
+
+require go.uber.org/multierr v1.10.0 // indirect
--- rest_errors/rest_errors.go
+++ rest_errors/rest_errors.go
@@ -1,45 +1,83 @@
 package rest_errors
 
 import (
-	"errors"
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
-type RestErr struct {
-	Message string `json:"message"`
-	Status  int    `json:"status"`
-	Error   string `json:"error"`
-	Causes []interface{} `json:"causes"`
+type RestErr interface {
+	Message() string
+	Status() int
+	Error() string
+	Causes() []any
 }
 
-func NewError(msg string) error {
-	return errors.New(msg)
+type restErr struct {
+	message string
+	status  int 
+	error   string 
+	causes []any 
 }
 
-func NewBadRequestError(message string) * RestErr {
-	return &RestErr{
-		Message: message,
-		Status: http.StatusBadRequest,
-		Error: "bad request",
+func (e restErr) Message() string{
+	return e.message
+}
+
+func (e restErr) Status() int{
+	return e.status
+}
+
+func (e restErr) Causes() []any {
+	return e.causes
+}
+
+func (e restErr) Error() string {
+	return fmt.Sprintf("message: %s - status: %d - error: %s - causes: [ %v ]", 
+	e.message, e.status, e.error, e.causes)
+}
+
+func NewRestError(message string, status int, err string, causes []any) RestErr {
+	return restErr{
+		message: message,
+		status: status,
+		error: err,
+		causes: causes,
 	}
 }
 
-func NewNotFoundError(message string) * RestErr {
-	return &RestErr{
-		Message: message,
-		Status: http.StatusNotFound,
-		Error: "not found",
+func NewBadRequestError(message string) RestErr {
+	return restErr{
+		message: message,
+		status: http.StatusBadRequest,
+		error: "bad request",
 	}
 }
 
-func NewInternalServerError(message string, err error) *RestErr {
-	result := &RestErr{
-		Message: message,
-		Status: http.StatusInternalServerError,
-		Error: "internal server error",
+func NewNotFoundError(message string) RestErr {
+	return restErr{
+		message: message,
+		status: http.StatusNotFound,
+		error: "not found",
+	}
+}
+
+func NewInternalServerError(message string, err error) RestErr {
+	result := restErr{
+		message: message,
+		status: http.StatusInternalServerError,
+		error: "internal server error",
 	}
 	if err != nil {
-		result.Causes = append(result.Causes, err.Error())
+		result.causes = append(result.causes, err.Error())
 	}
 	return result
+}
+
+func NewRestErrorFromBytes(bytes []byte) (RestErr, error) {
+	var apiErr restErr
+	if err := json.Unmarshal(bytes, &apiErr); err != nil {
+		return nil, err
+	}
+	return apiErr, nil
 }
